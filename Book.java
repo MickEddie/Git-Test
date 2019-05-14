86d4a71361148306860ea6931ada532b2c4e0ad5
MickEddie <michaelokandeji@yahoo.co.uk>
2019-05-14 16:13:49 +0100

    Update2 commit

--- Book.java
+++ Book.java
@@ -4,8 +4,21 @@
 
 	private boolean isHardcover;
 	private String aurthur;
-	private int numberofages; 
+	private int numberofpages; 
 	private String publisher;
+
+	public void setaurthur(String aurthur) {
+		this.aurthur = aurthur;
+	}
+	public String getaurthur() {
+		return aurthur;
+	}
+	public void setnumberofpages(int numberofpages) {
+		this.numberofpages = numberofpages;
+	}
+	public int getnumberofpages() {
+		return numberofpages;
+	}
 
 
 
